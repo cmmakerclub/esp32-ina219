4631f22c4aee267bf1ae1be023ea68f63e250cd1
bavensky <apiruk_sang-ngenchai@hotmail.com>
2019-01-25 13:35:02 +0700

    cal watt-hour

--- esp32-ina219-serialout/esp32-ina219-serialout.ino
+++ esp32-ina219-serialout/esp32-ina219-serialout.ino
@@ -8,23 +8,21 @@ HardwareSerial Serial1(2); // RX2(16)  TX2(17)
 
 float shuntvoltage, busvoltage, loadvoltage;
 double current_mA, current_A, sumCurrent;
-float power_mW = 0, power_W = 0, energy = 0;
-float ampHours = 0.0, wattHours = 0.0;
-uint32_t pevTime = 0, msec = 0, time;
-uint32_t energyConsumed = 0;
+float power_mW = 0, power_W = 0;
+uint32_t pevTime = 0, pevPrint = 0;
 uint32_t powerCountt = 0;
 double powerSum = 0;
 double powerAverage = 0;
-double powerHour = 0;
-uint32_t pevPrint = 0;
+double powerHour = 0, powerWattHour, powerKilosWattHour;
 
 void setup(void)
 {
-  Wire.begin(21, 22);
-
+  Wire.begin(21, 22); 
+  
   Serial.begin(115200);
   Serial1.begin(115200, SERIAL_8N1, 16 /*rx*/, 17 /* tx */);
-  Serial.println("INA219 begin..");
+
+  // init ina219
   ina219.begin();
 }
 
@@ -47,16 +45,31 @@ void loop(void)
     Serial.print("Voltage     : "); Serial.print(busvoltage); Serial.println(" V");
     Serial.print("Current mA  : "); Serial.print(current_mA); Serial.println(" mA");
     Serial.print("Power mW    : "); Serial.print(power_mW);   Serial.println(" mW");
-    Serial.print("Power sum   : "); Serial.println(powerSum); Serial.println(" mWS");
+    Serial.print("Power sum   : "); Serial.print(powerSum); Serial.println(" mWS");
 
-    powerAverage = powerSum / powerCountt; // W / mS
-    Serial.print("Power ave   : "); Serial.println(powerAverage); Serial.println(" W");
+    powerAverage = powerSum / powerCountt; // mWS / mS
+    Serial.print("Power ave   : "); Serial.print(powerAverage); Serial.println(" W");
 
-    powerHour =  powerAverage * 3600;
-    Serial.print("Power Hour  : "); Serial.print(powerHour);   Serial.print(" mWH");   
+    powerHour =  powerAverage * 3600; // convert to hour
+    Serial.print("Power Hour  : "); Serial.print(powerHour);   Serial.println(" mWH");
+
+    powerWattHour =  powerHour / 1000; // convert to watt-hour
+    Serial.print("Power Wh    : "); Serial.print(powerWattHour);   Serial.println(" WH");
+
+    powerKilosWattHour =  powerWattHour / 1000; // convert to kilo-watt-hour
+    Serial.print("Power kWh   : "); Serial.print(powerKilosWattHour);   Serial.println(" kWH");
 
     Serial.println("  ");
     Serial.println("  ");
-    powerCountt = 0;
+    
+    powerCountt = 0;  // clear count sum watts
+
+    Serial1.print("Volt=");
+    Serial1.print(busvoltage);
+    Serial1.print(",Current=");
+    Serial1.print(current_mA);
+    Serial1.print(",Power=");
+    Serial1.print(powerWattHour);
+    Serial1.print("\n");
   }
 }
